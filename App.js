801dabd711cafbf016c05abe75b5db275ddb589a
Gaurav-Salian <gauravsalian11@gmail.com>
2024-08-10 19:56:27 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View, Text, StatusBar, Platform } from 'react-native';
+import { WebView } from 'react-native-webview';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <StatusBar barStyle="dark-content" />
+//       <Text>Hello, World!</Text>
+//       {/* <WebView source={{ uri: 'http://dc1.progenerp.com:8008/erpsm/app_erpsmLogin/' }} style={{ flex: 1 }} /> */}
+//     </View>
+//   );
+// }
+
+const mywebview = () => {
+  if (Platform.OS === 'web'){
+    return <Text>Not supported on web</Text>
+  // }else {    return <Text>  \t  Is supported on web</Text>
 }
+  return (
+    <WebView
+      source={{ uri: 'http://dc1.progenerp.com:8008/erpsm/app_erpsmLogin/' }} style={{marginTop:20}}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+      />
+  );
+};
+
+export default mywebview;
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//   },
+// });
